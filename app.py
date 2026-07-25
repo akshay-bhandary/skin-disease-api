3627b309d895082c3e1fd490a4a80a7a885b0b17
akshay-bhandary <bhandaryakshay68@gmail.com>
2026-07-25 17:59:52 +0530

    Implement skin disease detection API with Flask

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, jsonify
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+import numpy as np
+import os
+
+app = Flask(__name__)
+model = load_model('skin_disease_model.h5')
+
+class_names = ['Atopic Dermatitis', 'Basal Cell Carcinoma', 'Melanocytic Nevi',
+               'Melanoma', 'Warts Molluscum', 'Eczema', 'Benign Keratosis']
+
+@app.route('/')
+def home():
+    return "Skin Disease Detection API is running"
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    file = request.files['image']
+    img_path = 'temp.jpg'
+    file.save(img_path)
+
+    img = image.load_img(img_path, target_size=(224, 224))
+    img_array = image.img_to_array(img) / 255.0
+    img_array = np.expand_dims(img_array, axis=0)
+
+    prediction = model.predict(img_array)
+    predicted_class = class_names[np.argmax(prediction)]
+    confidence = float(np.max(prediction))
+
+    os.remove(img_path)
+    return jsonify({'prediction': predicted_class, 'confidence': confidence})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
